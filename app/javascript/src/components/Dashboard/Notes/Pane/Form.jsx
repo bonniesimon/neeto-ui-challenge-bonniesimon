e728ee2adf7fc7dd13064f447e61bb19312cb980
Bonnie Simon <bonnie.developer@gmail.com>
2022-09-15 13:05:54 +0530

    Add reset to cancel button

--- app/javascript/src/components/Dashboard/Notes/Pane/Form.jsx
+++ app/javascript/src/components/Dashboard/Notes/Pane/Form.jsx
@@ -95,6 +95,7 @@ const NoteForm = ({ onClose, refetch, note, isEdit }) => {
               label="Cancel"
               size="large"
               style="text"
+              type="reset"
               onClick={onClose}
             />
           </Pane.Footer>
